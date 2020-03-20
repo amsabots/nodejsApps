b2529a496b9b3b95e6e1489a5f8dc807cc4d6e36
DiscoveryCity <amsabots@gmail.com>
2020-03-20 23:52:42 +0300

    second commit

--- controllers/sms.js
+++ controllers/sms.js
@@ -1,18 +1,18 @@
-const { getClient } = require("../controllers/account");
-module.exports = function SendSms(twilio, message) {
-  return new Promise((resolve, reject) => {
-    getClient(twilio).then(user => {
-      let { sid, token, number } = user.sms;
-      const client = require("twilio")(sid, token);
-      client.messages
-        .create({
-          to: twilio.From.split(":")[1],
-          body: message,
-          statusCallback: "http://f6a1d4b1.ngrok.io/sms/status",
-          from: number
-        })
-        .then(message => resolve(message))
-        .catch(err => reject(err));
-    });
-  });
-};
+// const { getClient } = require("../controllers/account");
+// module.exports = function SendSms(twilio, message) {
+//   return new Promise((resolve, reject) => {
+//     getClient(twilio).then(user => {
+//       let { sid, token, number } = user.sms;
+//       const client = require("twilio")(sid, token);
+//       client.messages
+//         .create({
+//           to: twilio.From.split(":")[1],
+//           body: message,
+//           statusCallback: "http://f6a1d4b1.ngrok.io/sms/status",
+//           from: number
+//         })
+//         .then(message => resolve(message))
+//         .catch(err => reject(err));
+//     });
+//   });
+// };
